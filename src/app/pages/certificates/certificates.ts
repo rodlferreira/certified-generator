237933ffd555178cfcf90b3511b1dcd2e49304e2
rodlferreira <rodriggo.lferreira@icloud.com>
2026-01-10 22:34:56 +0000

    feat: show certified created

---
+++ src/app/pages/certificates/certificates.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {SecondaryButton} from '../../_components/secondary-button/secondary-button';
+
+@Component({
+  selector: 'app-certificates',
+  imports: [
+    SecondaryButton
+  ],
+  templateUrl: './certificates.html',
+  styleUrl: './certificates.css',
+})
+export class Certificates {
+
+}
