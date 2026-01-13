631be9fb30215ca052da9ad05d0be04d1a1b1768
rodlferreira <rodriggo.lferreira@icloud.com>
2026-01-13 21:30:00 +0000

    feat: implemented ngModel on certified-form

--- src/app/pages/certified-form/certified-form.ts
+++ src/app/pages/certified-form/certified-form.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import {SecondaryButton} from '../../_components/secondary-button/secondary-button';
 import {PrimaryButton} from '../../_components/primary-button/primary-button';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-certified-form',
   imports: [
     SecondaryButton,
-    PrimaryButton
+    PrimaryButton,
+    FormsModule
   ],
   templateUrl: './certified-form.html',
   styleUrl: './certified-form.css',
 })
 export class CertifiedForm {
-
+nome: string = "";
+atividade: string = "";
+atividades: string[] = ['Angular', 'React']
 }
