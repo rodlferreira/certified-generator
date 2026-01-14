7f7f41d7942f0dc6a96fb94e7a1bc294275aea61
rodlferreira <rodriggo.lferreira@icloud.com>
2026-01-14 22:17:35 +0000

    field-form validation

--- src/app/pages/certified-form/certified-form.ts
+++ src/app/pages/certified-form/certified-form.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import {SecondaryButton} from '../../_components/secondary-button/secondary-button';
 import {PrimaryButton} from '../../_components/primary-button/primary-button';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, NgModel} from '@angular/forms';
+import {required} from '@angular/forms/signals';
+import {NgClass, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-certified-form',
   imports: [
     SecondaryButton,
     PrimaryButton,
-    FormsModule
+    FormsModule,
+    NgStyle,
+    NgClass
   ],
   templateUrl: './certified-form.html',
   styleUrl: './certified-form.css',
@@ -17,4 +21,9 @@ export class CertifiedForm {
 nome: string = "";
 atividade: string = "";
 atividades: string[] = ['Angular', 'React']
+
+  campoValido(control: NgModel) {
+    return control.invalid && control.touched && control.errors
+  }
+
 }
